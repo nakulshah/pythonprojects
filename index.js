c616cc05973d95da8f58128303f53fd984e05b87
Nakul Shah <nakulshah@gmail.com>
2024-07-22 22:09:25 -0500

    sorting, pagination and search

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 new gridjs.Grid({
   columns: ["date", "station_name", "day_type", "rides"],
+  pagination: true,
+  search: true,
+  sort: true,
   server: {
     url: 'http://127.0.0.1:5000/ctalrides',
     then: data => data
